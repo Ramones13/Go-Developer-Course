da42f48d6e3b65d44681ec511b0aee43b952a5a6
Ilgam <Gn0m_13@mail.ru>
2024-03-30 20:46:51 +0300

    3-5

---
+++ lesson3/task3-5/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const (
+		const1 = 1
+		const2 = 2
+		const3 = 3
+		const4 = 4
+		const5 = 5
+	)
+	
+	fmt.Printf("Const 1 = %v, ", const1)
+	fmt.Printf("Const 2 = %v, ", const2)
+	fmt.Printf("Const 3 = %v, ", const3)
+	fmt.Printf("Const 4 = %v, ", const4)
+	fmt.Printf("Const 5 = %v, ", const5)
+
+}
